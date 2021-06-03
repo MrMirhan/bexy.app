e1458bb1373293f15a01d4d17041ebbcb4dd8687
Mirhan <40675708+MrMirhan@users.noreply.github.com>
2021-06-03 07:52:56 -0700

    Update loggerM.py

--- loggerM.py
+++ loggerM.py
@@ -1,4 +1,5 @@
 import logging, datetime, time, os, sys
+import config as cf
 from re import search
 
 date = str(datetime.datetime.now())
@@ -6,15 +7,15 @@ todayDate = str(date.split(" ")[0])
 nowTime = str(date.split(" ")[1]).split(":")
 nowTime = str(nowTime[0]) + "." + nowTime[1] + "." + str(round(float(nowTime[2])))
 try:
-    os.makedirs(f"logs/alpha/{todayDate}")    
+    os.makedirs(f"logs/" + cf.branch + f"/{todayDate}")    
     print("Todays logging directory " , todayDate ,  " created.")
-    open(f"logs/alpha/{todayDate}/{nowTime}.log", "w")
-    logging.basicConfig(filename=f"logs/alpha/{todayDate}/{nowTime}.log", filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    open(f"logs/" + cf.branch + f"/{todayDate}/{nowTime}.log", "w")
+    logging.basicConfig(filename=f"logs/" + cf.branch + f"/{todayDate}/{nowTime}.log", filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     logging.warning('Process started.')
 except FileExistsError:
     print("This process started time " , nowTime ,  " logging file created.")  
-    open(f"logs/alpha/{todayDate}/{nowTime}.log", "w")
-    logging.basicConfig(filename=f"logs/alpha/{todayDate}/{nowTime}.log", filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    open(f"logs/" + cf.branch + f"/{todayDate}/{nowTime}.log", "w")
+    logging.basicConfig(filename=f"logs/" + cf.branch + f"/{todayDate}/{nowTime}.log", filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     logging.warning('Process started.')
 
 def sendLog(message):
